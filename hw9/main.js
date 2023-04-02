be3bad5402a9a4773a0f5efca903f7aa23ef0c78
DanBrazhnyk <danylo.brazhnyk.com>
2023-04-02 13:35:35 +0200

    hw9

--- hw9/main.js
+++ hw9/main.js
@@ -1,15 +1,35 @@
-const yourFunc = (arr) => {const mas= [...new Set(arr)]; console.log( mas)};
+const arr1 = [1,2,3,4,5,1,2,3,4,2,5,'b','b','b','b']
+const findUnique = (arr) => {const newArr= [...new Set(arr)]; console.log(newArr)};
+findUnique(arr1)
 
-const uniqeSymbol = (arr)=>{const mas = [];
-for(const elem of arr){
-    mas[elem]=elem
-}
-const newArr =Object.values(mas)
-console.log(newArr)}
+// const uniqeSymbol = (arr)=>{
+//   const mas = [];
+//   for(const elem of arr){
+//     mas[elem]=elem
+//   }
+//   const newArr =Object.values(mas)
+//   console.log(newArr)}
+// говно кодерское решение)
+const uniqueSymbols =(arr)=>arr.filter((item,index)=> index===arr.indexOf(item))
+console.log(uniqueSymbols(arr1))
 
-const firstSymbol=(arr)=>{arr.filter()}
-const arr1 = [1,2,3,4,5,1,2,3,4,5,'b']
-uniqeSymbol(arr1) 
+function mostFrequent(arr){
+  let mostFrequent = arr[0];
+  let counter = 1;
+  const findFrequentElem = arr.reduce((mas, elem) => {
+    if (elem in mas) {
+      mas[elem]++;
+    } else {
+      mas[elem] = 1;
+    }
+    if (mas[elem] > counter || (mas[elem] === counter && arr.indexOf(elem) < arr.indexOf(mostFrequent))) {
+      counter = mas[elem];
+      mostFrequent = elem;
+    }
+    return mas;
+  }, {});
+  return mostFrequent;}
+console.log(mostFrequent(arr1))
 
 let y = 5;
 let x = () => y;
@@ -22,6 +42,7 @@ console.log(z(x))
 //создаём переменную у равную 5 которую нигде не юзаем и параметр t который пытаемcя вызвать как функцию,
 //но так как t мы не возвращаем с помощью return оно выведет undefined,если сделать внутри return t();то все будет гуд) и выведется у который передавали в х,
 //такой нюанс произошел из-за того что юзаем фигурные скобочки и есть несколько выражений и стрелочной функции нужно понять что мы хотим от нее
+
 function debounce(f, delay) {
   let time;
   return function(...elem) { 
@@ -29,8 +50,8 @@ function debounce(f, delay) {
       f.apply(this, elem);
       time = Date.now();
   } else {
-      let interwalTime = Date.now() - time;
-      if (interwalTime > delay) {
+      let intervalTime = Date.now() - time;
+      if (intervalTime > delay) {
         f.apply(this, elem);
         time = Date.now();
         }};
