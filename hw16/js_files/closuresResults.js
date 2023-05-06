31b3d88bb9a53c913bed994897b176978c699db5
Danylo Brazhnyk <danylo.brazhnyk@gmail.com>
2023-05-06 20:15:38 +0200

    edit

--- hw16/js_files/closuresResults.js
+++ hw16/js_files/closuresResults.js
@@ -1,6 +1,6 @@
 import { sum as getSum } from "./closuresSum.js";
 import { mult as getMult} from "./closuresMult.js";
-//Тут фдруг чего уже фантазия началась)
+//Тут вдруг чего уже фантазия началась)
 const price = getSum(150)(700)
 const procent = getMult(price,2)
 
